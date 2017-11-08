5248fd0d58389b1e833261a0473c430cd29adcce
Tomonori Hayashi <hyshtmnr6238@gmail.com>
2017-11-09 08:59:39 +0900

    add batch

---
+++ cron/make_list.py
@@ -0,0 +1,24 @@
+import os
+"""
+files = os.listdir('../src/ver1/dlPDF')
+count = 0
+with open('../src/ver1/PDF_list.txt', 'w') as f:
+    for name in files:
+        if '.zip' in name:
+            f.write(name[:-8])
+            if count < len(files)-2:
+                f.write('\n')
+        count += 1
+
+"""
+import platform
+from smb.SMBConnection import SMBConnection
+
+conn = SMBConnection(
+        'administrator',
+        'jvb1l0Iwata',
+        platform.uname().node,
+        'yellow.wsl.mind.meiji.ac.jp',
+        domain='',
+        use_ntlm_v2=True)
+conn.connect('yellow.wsl.mind.meiji.ac.jp', 139)
